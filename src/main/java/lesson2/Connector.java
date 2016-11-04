a78f757ebdc76cf7a9a9a1c43251496acde5dfc2
Karginskiy <hei@spark-mail.ru>
2016-11-04 18:29:36 +0300

    Connector changed

--- src/main/java/lesson2/Connector.java
+++ src/main/java/lesson2/Connector.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Connector {
+class Connector {
 
     private Connection connection;
     private String dbPath;
@@ -13,7 +13,7 @@ public class Connector {
         this.dbPath = dbPath;
     }
 
-    public void connect() {
+    void connect() {
         try {
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection(dbPath);
@@ -23,7 +23,7 @@ public class Connector {
         }
     }
 
-    public void disconnect() {
+    void disconnect() {
         try {
             connection.close();
         } catch (SQLException e) {
@@ -31,7 +31,7 @@ public class Connector {
         }
     }
 
-    public Connection getConnection() {
+    Connection getConnection() {
         return this.connection;
     }
 
