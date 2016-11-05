fb1c261f3bea07a1f2fc416b5680530927392161
Karginskiy <hei@spark-mail.ru>
2016-11-05 21:39:41 +0300

    Using of default synchronizing

---
+++ src/main/java/concurrency/SynchronizeExample.java
@@ -0,0 +1,37 @@
+package concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+import static concurrency.ConcurrentUtils.sleep;
+import static concurrency.ConcurrentUtils.stop;
+
+public class SynchronizeExample {
+
+    int count = 0;
+
+    synchronized void increment() {
+        count = count + 1;
+    }
+
+    public void thready() {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        IntStream.range(0, 10000)
+                .forEach(i -> executorService.submit(this::increment));
+
+        stop(executorService);
+
+        System.out.println(count);
+
+        // 9978 - result. Not 10000.
+
+    }
+
+    public static void main(String[] args) {
+
+        new SynchronizeExample().thready();
+
+    }
+
+}
