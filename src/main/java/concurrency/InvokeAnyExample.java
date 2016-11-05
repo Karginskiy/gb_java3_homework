eade92a37ec6feff05fab24824024d21d2a9fd8e
Karginskiy <hei@spark-mail.ru>
2016-11-05 21:36:39 +0300

    Using of InvokeAny

---
+++ src/main/java/concurrency/InvokeAnyExample.java
@@ -0,0 +1,32 @@
+package concurrency;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class InvokeAnyExample {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newWorkStealingPool();
+
+        List<Callable<String>> callables = Arrays.asList(
+                callable("task1", 2),
+                callable("task2", 1),
+                callable("task3", 3));
+
+        try {
+            String result = executorService.invokeAny(callables);
+            System.out.println(result);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Callable callable(String result, long sleepSeconds) {
+        return () -> {
+            TimeUnit.SECONDS.sleep(sleepSeconds);
+            return result;
+        };
+    }
+
+}
