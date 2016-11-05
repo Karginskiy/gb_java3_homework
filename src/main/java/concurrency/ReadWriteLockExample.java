3228e89980da331679c22526611112ccff8840f2
Karginskiy <hei@spark-mail.ru>
2016-11-05 21:37:10 +0300

    Using of Lockers from ConcurrencyAPI

---
+++ src/main/java/concurrency/ReadWriteLockExample.java
@@ -0,0 +1,45 @@
+package concurrency;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import static concurrency.ConcurrentUtils.sleep;
+
+public class ReadWriteLockExample {
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Map<String, String> map = new HashMap<>();
+        ReadWriteLock lock = new ReentrantReadWriteLock();
+
+        executorService.submit(() -> {
+            lock.writeLock().lock();
+            try {
+                sleep(1);
+                map.put("foo", "bar");
+            } finally {
+                lock.writeLock().unlock();
+            }
+        });
+
+        Runnable readTask = () -> {
+            lock.readLock().lock();
+            try {
+                System.out.println(map.get("foo"));
+                sleep(1);
+            } finally {
+                lock.readLock().unlock();
+            }
+        };
+
+        executorService.submit(readTask);
+        executorService.submit(readTask);
+
+    }
+
+}
