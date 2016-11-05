fe80ff0905cf1ed328647a2ab5d5131cd1a6eec0
Karginskiy <hei@spark-mail.ru>
2016-11-05 21:39:21 +0300

    StampedLock using

---
+++ src/main/java/concurrency/StampedLockExample.java
@@ -0,0 +1,45 @@
+package concurrency;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.StampedLock;
+
+import static concurrency.ConcurrentUtils.sleep;
+
+public class StampedLockExample {
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Map<String, String> map = new HashMap<>();
+        StampedLock lock = new StampedLock();
+
+        executorService.submit(() -> {
+
+            long stamp = lock.writeLock();
+            try {
+                sleep(1);
+                map.put("foo", "bar");
+            } finally {
+                lock.unlockWrite(stamp);
+            }
+
+        });
+
+        Runnable readTask = () -> {
+            long stamp = lock.readLock();
+            try {
+                System.out.println(map.get("foo"));
+                sleep(1);
+            } finally {
+                lock.unlockRead(stamp);
+            }
+        };
+
+        executorService.submit(readTask);
+
+    }
+
+}
